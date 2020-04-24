5d0b2882bc9c8e1fe9283cdb30cb318ab7aab8f3
jbsaeza <jbsaez@uc.cl>
2020-04-24 01:02:30 +0000

    new repo

---
+++ red.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+  
+struct node{
+  int id;
+  int *link_ids;
+  int links;
+};
+int dat[2];
+char palabras1[50];
+char palabras2[50];
+char palabras3[50];
+char palabras4[50];
+char palabras5[50];
+char palabras6[50];
+
+
+struct node *nodes;
+int number_of_nodes;
+int number_of_links;
+
+void leer_archivo(char* filename);
+void guardar_nodos_ordenados(char* filename);
